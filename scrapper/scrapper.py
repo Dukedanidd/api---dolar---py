4b0bc024370d1103337d389f3d05ffd887960573
Duke <jd.daniel.contact@gmail.com>
2025-01-08 17:09:24 -0600

    Refactor scrapper to extract currency values from iframe

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,25 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL de la pagina web que queremos scrapear
-url = 'https://www.bbva.mx/personas/informacion-financiera-al-dia.html'
+# URL del iframe
+iframe_url = 'https://bbv.infosel.com/bancomerindicators/indexV9.aspx'
 
-# Realizamos la peticion HTTP a la URL que usamos
-response = requests.get(url)
+# Realizamos la solicitud HTTP al iframe
+response = requests.get(iframe_url)
 
-# Vamos a verificar si la peticion fue buena con un if
 if response.status_code == 200:
-    # Aqui voy a analizar el contenido de la respuesta
+    # Analizamos el contenido del iframe
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Extraemos los valores de compra y venta
-    compra = soup.find('span', class_='text-right precio-c').text.strip()
-    venta = soup.find('span', class_='text-left precio-c').text.strip()
-    
-    # Imprimimos los valores
-    print(f"Compra: {compra}")
-    print(f"Venta: {venta}")
+    # Usamos los selectores CSS para extraer los valores de compra y venta
+    try:
+        compra = soup.select_one('body > section.indicadores.bg-divisas > div:nth-child(1) > div > div > div > div:nth-child(1) > div.d-flex > div.col.border-right > div:nth-child(2) > span').text.strip()
+        venta = soup.select_one('body > section.indicadores.bg-divisas > div:nth-child(1) > div > div > div > div:nth-child(1) > div.d-flex > div:nth-child(2) > div:nth-child(2) > span').text.strip()
+
+        # Imprimimos los valores
+        print(f"Compra: {compra}")
+        print(f"Venta: {venta}")
+    except AttributeError:
+        print("No se encontraron los valores dentro del iframe.")
 else:
-    print('No se pudo obtener la informacion')
-
-
+    print("No se pudo obtener la información del iframe.")
